25841708e98d21852262c5d10a7b9385725289dd
Jordan Sherer <jordan@widefido.com>
2016-01-27 09:44:24 -0500

    Added utility transform for first/last/random selection

---
+++ transformer/transforms/util/choose_test.py
@@ -0,0 +1,42 @@
+import unittest
+import choose
+
+class TestUtilChooseTransform(unittest.TestCase):
+    def test_choose_empty(self):
+        transformer = choose.UtilChooseTransform()
+        self.assertEqual(0, transformer.transform_many([], options={'operation': 'first', 'default':0}))
+        self.assertEqual(0, transformer.transform_many([], options={'operation': 'last', 'default':0}))
+        self.assertEqual(0, transformer.transform_many([], options={'operation': 'random', 'default':0}))
+
+        self.assertEqual(0, transformer.transform_many([0], options={'operation': 'first', 'default':1}))
+        self.assertEqual(0, transformer.transform_many([0], options={'operation': 'last', 'default':1}))
+        self.assertEqual(0, transformer.transform_many([0], options={'operation': 'random', 'default':1}))
+
+        self.assertEqual(0, transformer.transform_many([0, 0], options={'operation': 'first', 'default':1}))
+        self.assertEqual(0, transformer.transform_many([0, 0], options={'operation': 'last', 'default':1}))
+        self.assertEqual(0, transformer.transform_many([0, 0], options={'operation': 'random', 'default':1}))
+
+        self.assertEqual(0, transformer.transform_many([''], options={'operation': 'first', 'default':0}))
+        self.assertEqual(0, transformer.transform_many([''], options={'operation': 'last', 'default':0}))
+        self.assertEqual(0, transformer.transform_many([''], options={'operation': 'random', 'default':0}))
+
+        self.assertEqual(0, transformer.transform_many(['', ''], options={'operation': 'first', 'default':0}))
+        self.assertEqual(0, transformer.transform_many(['', ''], options={'operation': 'last', 'default':0}))
+        self.assertEqual(0, transformer.transform_many(['', ''], options={'operation': 'random', 'default':0}))
+
+    def test_choose_full(self):
+        transformer = choose.UtilChooseTransform()
+
+        self.assertEqual(1, transformer.transform_many([1, 2, 3], options={'operation': 'first', 'default':0}))
+        self.assertEqual(3, transformer.transform_many([1, 2, 3], options={'operation': 'last', 'default':0}))
+
+        # ensure we never get the default value...
+        for i in range(100):
+            self.assertNotEqual(0, transformer.transform_many([1, 2, 3], options={'operation': 'random', 'default':0}))
+            self.assertNotEqual('', transformer.transform_many(['', 1, 2, 3, 'a'], options={'operation': 'random', 'default':''}))
+
+        self.assertEqual(2, transformer.transform_many(['', 2, 3, ''], options={'operation': 'first', 'default':0}))
+        self.assertEqual(3, transformer.transform_many(['', 2, 3, ''], options={'operation': 'last', 'default':0}))
+
+        self.assertEqual('a', transformer.transform_many(['a', 2, 3, 'b'], options={'operation': 'first', 'default':0}))
+        self.assertEqual('b', transformer.transform_many(['a', 2, 3, 'b'], options={'operation': 'last', 'default':0}))

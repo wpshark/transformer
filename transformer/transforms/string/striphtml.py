6774e930a2e5d71601aa7f23e5d7063cf3c94647
Brian Cooksey <bac2k@hotmail.com>
2016-01-27 10:50:42 -0600

    Minor cleanup on strip html transform

--- transformer/transforms/string/striphtml.py
+++ transformer/transforms/string/striphtml.py
@@ -5,12 +5,11 @@ from bs4 import BeautifulSoup
 class StringStripHtmlTransform(BaseTransform):
 
     category = 'string'
-    name = 'strip html'
-    label = 'String / Strip HTML'
+    name = 'strip_html'
+    label = 'Remove HTML Tags'
     help_text = 'Remove every HTML tag to leave just the plain text.'
 
     def transform(self, str_input, **kwargs):
-        #return re.sub("<.*?>", "", str_input) if str_input else ''
         soup = BeautifulSoup(str_input, 'html.parser')
         return soup.get_text()
 
--- transformer/transforms/string/striphtml_test.py
+++ transformer/transforms/string/striphtml_test.py
@@ -4,6 +4,7 @@ import striphtml
 class TestStringStripHtmlTransform(unittest.TestCase):
     def test_striphtml(self):
         transformer = striphtml.StringStripHtmlTransform()
-        self.assertEqual(transformer.transform(""), "")
-        self.assertEqual(transformer.transform("<b>Bold</b>"), "Bold")
-        self.assertEqual(transformer.transform("< 3 donuts and > 4 pies"), "< 3 donuts and > 4 pies")
+        self.assertEqual(transformer.transform(''), '')
+        self.assertEqual(transformer.transform('<b>Bold</b>'), 'Bold')
+        self.assertEqual(transformer.transform('< 3 donuts and > 4 pies'), '< 3 donuts and > 4 pies')
+        self.assertEqual(transformer.transform('4 is &lt; 5'), u'4 is < 5')

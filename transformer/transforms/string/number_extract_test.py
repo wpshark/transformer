93850f091c16c0d889608b7e3525997024b60943
Brian Cooksey <bac2k@hotmail.com>
2016-01-29 13:44:45 -0600

    Add number extract transform  * Really dumb at the moment, but effective. Won't handle the case of multiple numbers very well though

---
+++ transformer/transforms/string/number_extract_test.py
@@ -0,0 +1,13 @@
+import unittest
+import number_extract
+
+class TestStringNumberExtractTransform(unittest.TestCase):
+    def test_number_extract(self):
+        transformer = number_extract.StringNumberExtractTransform()
+        self.assertEqual(transformer.transform(u''), u'')
+        self.assertEqual(transformer.transform(None), u'')
+        self.assertEqual(transformer.transform(u'I have no dogs'), u'')
+        self.assertEqual(transformer.transform(u'I have 3 dogs'), u'3')
+        self.assertEqual(transformer.transform(u'I have 3,124 dogs'), u'3,124')
+        self.assertEqual(transformer.transform(u'I have 3 124 dogs'), u'3 124')
+        self.assertEqual(transformer.transform(u'I have 3.124 dogs'), u'3.124')

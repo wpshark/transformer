0667aa5954ca2a8efd059b00e63f4e29747f74c3
Jordan Sherer <jordan@widefido.com>
2016-02-19 10:42:21 -0500

    Wording for split

--- transformer/transforms/string/split.py
+++ transformer/transforms/string/split.py
@@ -6,8 +6,8 @@ class StringSplitTransform(BaseTransform):
 
     category = 'string'
     name = 'split'
-    label = 'Split a String'
-    help_text = 'Split a string on a character or word and return a segment'
+    label = 'Split Text'
+    help_text = 'Split the text on a character or word and return a segment'
 
     noun = 'String'
     verb = 'split'
@@ -38,14 +38,14 @@ class StringSplitTransform(BaseTransform):
                 'required': False,
                 'key': 'separator',
                 'label': 'Separator',
-                'help_text': 'Character or word separator to split the string on. (Default: space)'
+                'help_text': 'Character or word separator to split the text on. (Default: space)'
             },
             {
                 'type': 'unicode',
                 'required': False,
                 'key': 'index',
                 'label': 'Segment Index',
-                'help_text': 'Segment to return after splitting. (Default: First)',
+                'help_text': 'Segment of text to return after splitting. (Default: First)',
                 'choices': '0|First,1|Second,-1|Last,-2|Second to Last,all|All',
             },
         ]

dc4aedd98de504df4f5e92e339d87c7846a00d49
Jordan Sherer <jordan@widefido.com>
2016-02-18 22:37:27 -0500

    Updated label and help text

--- transformer/transforms/string/split.py
+++ transformer/transforms/string/split.py
@@ -7,12 +7,12 @@ class StringSplitTransform(BaseTransform):
     category = 'string'
     name = 'split'
     label = 'Split a String'
-    help_text = 'Split a string and return a segment'
+    help_text = 'Split a string on a character or word and return a segment'
 
     noun = 'String'
     verb = 'split'
 
-    def transform(self, str_input, separator=u'', segment=0, **kwargs):
+    def transform(self, str_input, separator=u'', index=0, **kwargs):
         segment = try_parse_number(segment, cls=int)
 
         if separator:
@@ -21,7 +21,7 @@ class StringSplitTransform(BaseTransform):
             segments = str_input.split()
 
         try:
-            return segments[segment]
+            return segments[index]
         except:
             pass
 
@@ -34,13 +34,14 @@ class StringSplitTransform(BaseTransform):
                 'required': False,
                 'key': 'separator',
                 'label': 'Separator',
-                'help_text': 'Separator to split the string on. (Default: space)'
+                'help_text': 'Character or word separator to split the string on. (Default: space)'
             },
             {
                 'type': 'unicode',
                 'required': False,
-                'key': 'segment',
-                'label': 'String segment to return after splitting. (Default: First)',
+                'key': 'index',
+                'label': 'Segment Index',
+                'help_text': 'Segment to return after splitting. (Default: First)',
                 'choices': '0|First,1|Second,-1|Last,-2|Second to Last',
             },
         ]

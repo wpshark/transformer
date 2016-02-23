f3df6260ef7ce21d44b4c8208c208166673079b7
Bryan Helmig <bryan@bryanhelmig.com>
2016-02-23 09:49:00 -0800

    completely empty test for lookup

--- transformer/transforms/util/lookup_test.py
+++ transformer/transforms/util/lookup_test.py
@@ -14,3 +14,5 @@ class TestUtilLookupTransform(unittest.TestCase):
         self.assertEquals(transformer.transform(u'asfguy', table={'a': 1, u'b': 2}, fallback=3), 3)
         self.assertEquals(transformer.transform(u'', table={'a': 1, u'b': 2}, fallback=3), 3)
         self.assertEquals(transformer.transform(u'', table={'a': 1, u'b': 2, '': 10}, fallback=u'cat'), u'cat')
+
+        self.assertEquals(transformer.transform(u'nothing'), u'')

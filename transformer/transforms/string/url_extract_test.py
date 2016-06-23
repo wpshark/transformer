53947c065744c2a55c3d86f46b9f38a89d554adb
Jordan Sherer <jordan.sherer@zapier.com>
2016-06-23 16:42:47 -0400

    Added URL extract string transform

---
+++ transformer/transforms/string/url_extract_test.py
@@ -0,0 +1,12 @@
+import unittest
+import url_extract
+
+class TestStringURLExtractTransform(unittest.TestCase):
+    def test_urlextract(self):
+        transformer = url_extract.StringURLExtractTransform()
+        self.assertEqual(transformer.transform(""), "")
+        self.assertEqual(transformer.transform(None), "")
+        self.assertEqual(transformer.transform("my site is google.com"), "google.com")
+        self.assertEqual(transformer.transform("some www.google.com web address www.yahoo.com"), "www.google.com")
+        self.assertEqual(transformer.transform("some http://www.google.com web address www.yahoo.com"), "http://www.google.com")
+        self.assertEqual(transformer.transform("some <a href='http://www.google.com'>test</a> web address www.yahoo.com"), "http://www.google.com")

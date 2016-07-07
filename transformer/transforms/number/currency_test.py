16406c4a17aea785b815b34c0217c5d9b7c7beb4
Bruno Bernardino <bruno.bernardino@gmail.com>
2016-07-07 16:28:53 +0100

    Adding empty string test to currency

--- transformer/transforms/number/currency_test.py
+++ transformer/transforms/number/currency_test.py
@@ -15,3 +15,4 @@ class TestNumberCurrencyTransform(unittest.TestCase):
         self.assertEqual(u'CA$1,234.10', transformer.transform(1234.1, currency='CAD', currency_format=u'¤#,##0.00', currency_locale='en-US'))
         self.assertEqual(u'CA$1,234.10', transformer.transform(u'CA$1,234.10', currency='CAD', currency_format=u'¤#,##0.00', currency_locale='en-US'))
         self.assertEqual('', transformer.transform(None, currency='CAD', currency_format=u'¤#,##0.00', currency_locale='en-US'))
+        self.assertEqual('', transformer.transform('', currency='CAD', currency_format=u'¤#,##0.00', currency_locale='en-US'))

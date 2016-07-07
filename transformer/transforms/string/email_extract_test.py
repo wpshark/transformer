549a2a64916ab60c76e702a3035c8b0407155cd5
Brian Cooksey <bac2k@hotmail.com>
2016-07-07 14:53:35 -0500

    Add a couple more tests to the string extractors

--- transformer/transforms/string/email_extract_test.py
+++ transformer/transforms/string/email_extract_test.py
@@ -5,6 +5,7 @@ class TestStringEmailExtractTransform(unittest.TestCase):
     def test_emailextract(self):
         transformer = email_extract.StringEmailExtractTransform()
         self.assertEqual(transformer.transform(""), "")
-        self.assertEqual(transformer.transform("my email is thomas@hils.us"), "thomas@hils.us")
         self.assertEqual(transformer.transform(None), "")
+        self.assertEqual(transformer.transform("I do not have email"), "")
+        self.assertEqual(transformer.transform("my email is thomas@hils.us"), "thomas@hils.us")
         self.assertEqual(transformer.transform("my email is broken thomas@hils .com"), "")
--- transformer/transforms/string/url_extract_test.py
+++ transformer/transforms/string/url_extract_test.py
@@ -6,6 +6,7 @@ class TestStringURLExtractTransform(unittest.TestCase):
         transformer = url_extract.StringURLExtractTransform()
         self.assertEqual(transformer.transform(""), "")
         self.assertEqual(transformer.transform(None), "")
+        self.assertEqual(transformer.transform("I do not have a website"), "")
         self.assertEqual(transformer.transform("my site is google.com"), "google.com")
         self.assertEqual(transformer.transform("some www.google.com web address www.yahoo.com"), "www.google.com")
         self.assertEqual(transformer.transform("some http://www.google.com web address www.yahoo.com"), "http://www.google.com")

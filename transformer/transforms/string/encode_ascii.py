99d69643a134035b42bdd36f1acfdeb8e380a129
Fokke Zandbergen <mail@fokkezb.nl>
2017-10-10 11:44:08 +0200

    Use unidecode to preserve ö

--- transformer/transforms/string/encode_ascii.py
+++ transformer/transforms/string/encode_ascii.py
@@ -1,3 +1,5 @@
+from unidecode import unidecode
+
 from transformer.registry import register
 from transformer.transforms.base import BaseTransform
 
@@ -6,12 +8,12 @@ class StringEncodeasciiTransform(BaseTransform):
     category = 'string'
     name = 'encode_ascii'
     label = 'Convert to ASCII'
-    help_text = 'Removes all non-ASCII characters.'
+    help_text = 'Replace all non-ASCII characters.'
 
     noun = 'Text'
     verb = 'convert to ASCII'
 
     def transform(self, str_input, **kwargs):
-        return str_input.encode('ascii', 'ignore') if str_input else u''
+        return unidecode(str_input) if str_input else u''
 
 register(StringEncodeasciiTransform())
--- transformer/transforms/string/encode_ascii_test.py
+++ transformer/transforms/string/encode_ascii_test.py
@@ -5,5 +5,5 @@ class TestStringEncodeasciiTransform(unittest.TestCase):
     def test_encodeascii(self):
         transformer = encode_ascii.StringEncodeasciiTransform()
         self.assertEqual(transformer.transform(""), "")
-        self.assertEqual(transformer.transform(u'\u200cN\u200co\u200cv\u200c'), "Nov")
+        self.assertEqual(transformer.transform(u'ko\u017eu\u0161\u010dek'), "kozuscek")
         self.assertEqual(transformer.transform(None), "")

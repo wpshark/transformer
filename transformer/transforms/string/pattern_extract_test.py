cbebf003e8e1c74fe036e15be9a63a8c892b6cee
Fokke Zandbergen <mail@fokkezb.nl>
2017-12-03 21:12:27 +0100

    Fix test

--- transformer/transforms/string/pattern_extract_test.py
+++ transformer/transforms/string/pattern_extract_test.py
@@ -5,8 +5,8 @@ class TestStringPatternExtractTransform(unittest.TestCase):
     def test_pattern_extract(self):
         transformer = pattern_extract.StringPatternExtractTransform()
         pattern = "f([o]+)( b(?P<named>[a]+)r)?"
-        self.assertEqual(transformer.transform("", pattern), "")
-        self.assertEqual(transformer.transform(None, pattern), "")
+        self.assertEqual(transformer.transform("", pattern), {'_matched': False})
+        self.assertEqual(transformer.transform(None, pattern), {'_matched': False})
         self.assertEqual(transformer.transform(
             "Hello foo folks and another foo bar I will not catch", pattern),
             {0: 'oo', 1: None, 2: None, 'named': None, '_end': 9, '_start': 6, '_matched': True}

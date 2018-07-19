c3e4620790b2f4a5a07de5e9aea91381f0652735
Chris VanderKolk <christopher.n.vanderkolk@gmail.com>
2018-07-19 10:57:03 -0500

    Making helptext multiline

--- transformer/transforms/string/replace.py
+++ transformer/transforms/string/replace.py
@@ -7,7 +7,11 @@ class StringReplaceTransform(BaseTransform):
     category = 'string'
     name = 'replace'
     label = 'Replace'
-    help_text = 'Replace any character, word or phrase in the text with another character, word or phrase. Note that this is NOT a Replace All function; only the first character, word or phrase that is found will be replaced. For now, if you need to Replace All, you would have to do so in a Code Step.'
+    help_text = (
+        'Replace any character, word or phrase in the text with another character, word or phrase. '
+        'Note: This is _not_ a Replace All function; only the first character, word or phrase that '
+        'is found will be replaced.'
+    )
 
     noun = 'Text'
     verb = 'find and replace values within'

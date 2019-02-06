2b5860fbef9b0b9d3ba13fa216f8e01ea6dde7ea
Tim Schafli <tim.schafli@zapier.com>
2019-02-05 23:10:30 -0800

    Made changes that Chris V and I worked on. Worked on comments, variable naming, some optimization, and formatting (linted by Black). Added multi line docstring to the transform function. Changed the name of the transform to Line Itemizer.

--- transformer/transforms/util/string_to_lineitems.py
+++ transformer/transforms/util/string_to_lineitems.py
@@ -2,81 +2,113 @@ from transformer.registry import register
 from transformer.transforms.base import BaseTransform
 
 
-class UtilStringToLineItemsTransform(BaseTransform):
+class UtilLineItemizerTransform(BaseTransform):
 
-    category = 'util'
-    name = 'string_to_lineitems'
-    label = 'Text to Line-item(s)'
+    category = "util"
+    name = "line_itemizer"
+    label = "Create / Append / Prepend to Line-item(s)"
+    # Flesh this out to describe more functions
     help_text = (
-        'Convert comma delimited text to line-item(s). \'a,b,c,d\' becomes [a,b,c,d]. More on line-items '
-        '[here](https://zapier.com/help/formatter/#how-use-line-items-formatter).'
+        "Convert comma delimited text to line-item(s). 'a,b,c,d' becomes [a,b,c,d]. More on line-items "
+        "[here](https://zapier.com/help/formatter/#how-use-line-items-formatter)."
     )
-    
-    noun = 'Text'
-    verb = 'convert'
+
+    noun = "Text"
+    verb = "convert"
 
     def build_input_field(self):
         return {
-            'type': 'unicode',
-            'required': False,
-            'key': 'inputs',
-            'label': 'Line-item(s) Group Name',
-            'help_text': 'Optional name for your set of line-item(s). ex: "Orders", "Sale lines", etc. Default is "Line-item(s)".'
+            "type": "unicode",
+            "required": False,
+            "key": "inputs",
+            "label": "Line-item(s) Group Name",
+            "help_text": 'Optional name for your set of line-item(s). ex: "Orders", "Sale lines", etc. Default is "Line-item(s)".',
         }
 
-    def transform(self, input_key, table={}, **kwargs):
-        
-        #make sure that a name is set for the line-item group
-        if not input_key:
-            input_key = u'Line-item(s)'
+    def transform(self, input_key, my_dict={}, **kwargs):
+        """Take a dict input and output an array of one or more Zapier standard line-items.
 
-        #initialize output and separate lists dicts
+        Example:
+        User inputs a string and a dict like this:
+            input_key = "Order Lines"
+            my_dict = { 
+                "Price": "5,3.5,4",
+                "Description": "Hat,Shoes,Shirt",
+                "Quantity": "1,2,1"
+            }
+        Expected output:
+            {
+                Order Lines": [
+                    {
+                        "Price": "5",
+                        "Description": "Hat",
+                        "Quantity": "1"
+                    },
+                    {
+                        "Price": "3.5",
+                        "Description": "Shoes",
+                        "Quantity": "2"
+                    },
+                    {
+                        "Price": "4",
+                        "Description": "Shirt",
+                        "Quantity": "1"
+                    }
+                ]
+            }
+        """
+
+        # make sure that a name is set for the line-item group
+        if not input_key:
+            input_key = "Line-item(s)"
+
+        # initialize output and other variables
         output = {input_key: []}
-        separate_lists = {}
         longest_array = 0
 
-        #filter out entries with no key
-        table.pop('', None)
+        # filter out entries with no key
+        my_dict.pop("", None)
 
-        #split each string value in the dict by ',' and determine the 
-        #  length oflongest array
-        for k, v in table.iteritems():
-            separate_lists[k] = v.split(',')
-            if len(separate_lists[k]) > longest_array:
-                longest_array = len(separate_lists[k])
+        # split each string value in the dict by ',' and determine the
+        #  length of longest array
+        for k, v in my_dict.items():  # might not need iteritems
+            my_dict[k] = v.split(",")
+            if len(my_dict[k]) > longest_array:
+                longest_array = len(my_dict[k])
 
-        #make mini objects to add to the output object
-        for num in range(0,longest_array):
-            #initialize single line item
+        # make individual objects for each line (example: could include 1 value each of qty, description, price) to add to the output object
+        for num in range(0, longest_array):
+            # initialize a new individual line-item
             this_line_item = {}
-            for i, (k, v) in enumerate(table.items()):
-                #try to add each property from the Dict / table to the single line item. Skips if no property is available.
+            for k, v in my_dict.items():
+                # Try to add each property from my_dict to the individual line-item.
+                #  Skips if the list isn't long enough for an available property.
                 try:
-                    this_line_item.update({k: separate_lists[k][num]})
-                except: 
-                    n = 0 
-            #try adding single line item object to the main output array
+                    this_line_item.update({k: v[num]})
+                except Exception as e:
+                    pass
+            # try adding the individual line item object to the main output array. Skips if no object is available (unlikely).
             try:
                 output[input_key].append(this_line_item)
-            except:
-                n = 0
-            
+            except Exception as e:
+                pass
+
         return output
 
     def fields(self, *args, **kwargs):
         return [
             {
-                'type': 'dict',
-                'required': False,
-                'key': 'table',
-                'label': 'Line-item(s)',
-                'help_text': 'Line-item property names on the left (ex: Price, Description) and comma-separated '
-                    'values on the right. Each property must have a unique name. Properties without a name are '
-                    'ignored. Line-items mapped into the fields on the right will be treated as comma-separated '
-                    'text along with any plain text or single values you add. This can be used to append or '
-                    'prepend values to existing line-items.'
+                "type": "dict",
+                "required": False,
+                "key": "my_dict",
+                "label": "Line-item(s)",
+                "help_text": "Line-item property names on the left (ex: Price, Description) and comma-separated "
+                "values on the right. Each property must have a unique name. Properties without a name are "
+                "ignored. Line-items mapped into the fields on the right will be treated as comma-separated "
+                "text along with any plain text or single values you add. This can be used to append or "
+                "prepend values to existing line-items.",
             }
         ]
 
 
-register(UtilStringToLineItemsTransform())
+register(UtilLineItemizerTransform())
--- transformer/transforms/util/string_to_lineitems_test.py
+++ transformer/transforms/util/string_to_lineitems_test.py
@@ -1,39 +1,75 @@
 import unittest
 import string_to_lineitems
 
-class TestUtilStringToLineItemsTransform(unittest.TestCase):
 
+class TestUtilLineItemizerTransform(unittest.TestCase):
     def test_string_to_lineitems(self):
-        transformer = string_to_lineitems.UtilStringToLineItemsTransform()
-        self.assertEqual({u'test lines': [{u'price':'a'},{u'price':'b'},{u'price':'c'},
-            {u'price':'d'}]}, transformer.transform(u'test lines', table={u'price':'a,b,c,d'}))
-        self.assertEqual({u'Line-item(s)': [{u'price':'a'},{u'price':'b'},{u'price':'c'},
-            {u'price':'d'}]}, transformer.transform('', table={u'price':'a,b,c,d'}))
-        self.assertEqual({u'Line-item(s)': 
-            [{u'price':'a', u'name':'one'},
-            {u'price':'b', u'name':'two'},
-            {u'price':'c', u'name':'three'},
-            {u'price':'d', u'name':'four'}]},
-            transformer.transform('', table={u'price':'a,b,c,d', u'name':'one,two,three,four'}))
+        transformer = string_to_lineitems.UtilLineItemizerTransform()
+        self.assertEqual(
+            {
+                "test lines": [
+                    {"price": "a"},
+                    {"price": "b"},
+                    {"price": "c"},
+                    {"price": "d"},
+                ]
+            },
+            transformer.transform("test lines", my_dict={"price": "a,b,c,d"}),
+        )
+        self.assertEqual(
+            {
+                "Line-item(s)": [
+                    {"price": "a"},
+                    {"price": "b"},
+                    {"price": "c"},
+                    {"price": "d"},
+                ]
+            },
+            transformer.transform("", my_dict={"price": "a,b,c,d"}),
+        )
+        self.assertEqual(
+            {
+                "Line-item(s)": [
+                    {"price": "a", "name": "one"},
+                    {"price": "b", "name": "two"},
+                    {"price": "c", "name": "three"},
+                    {"price": "d", "name": "four"},
+                ]
+            },
+            transformer.transform(
+                "", my_dict={"price": "a,b,c,d", "name": "one,two,three,four"}
+            ),
+        )
 
     def test_string_to_lineitems_variable_length(self):
-        transformer = string_to_lineitems.UtilStringToLineItemsTransform()
-        self.assertEqual({u'Line-item(s)': 
-            [{u'price':'a', u'name':'one'},
-            {u'price':'b', u'name':'two'},
-            {u'price':'c', u'name':'three'},
-            {u'price':'d'}]},
-            transformer.transform('', table={u'price':'a,b,c,d', u'name':'one,two,three'}))
+        transformer = string_to_lineitems.UtilLineItemizerTransform()
+        self.assertEqual(
+            {
+                "Line-item(s)": [
+                    {"price": "a", "name": "one"},
+                    {"price": "b", "name": "two"},
+                    {"price": "c", "name": "three"},
+                    {"price": "d"},
+                ]
+            },
+            transformer.transform(
+                "", my_dict={"price": "a,b,c,d", "name": "one,two,three"}
+            ),
+        )
 
     def test_string_to_lineitems_empty(self):
-        transformer = string_to_lineitems.UtilStringToLineItemsTransform()
-        self.assertEqual({u'Line-item(s)':[]}, transformer.transform('', table={}))
-        self.assertEqual({u'Line-item(s)': 
-            [{u'price':'a', u'name':''},
-            {u'price':' ', u'name':'two'}]},
-            transformer.transform('', table={u'price':'a, ', u'name':',two'}))        
-        self.assertEqual({u'Line-item(s)': 
-            [{u'name':'one'},
-            {u'name':'two'}]},
-            transformer.transform('', table={u'':'a,b', u'name':'one,two'}))
-        
+        transformer = string_to_lineitems.UtilLineItemizerTransform()
+        self.assertEqual({"Line-item(s)": []}, transformer.transform("", my_dict={}))
+        self.assertEqual(
+            {
+                "Line-item(s)": [
+                    {"price": "a", "name": ""},
+                    {"price": " ", "name": "two"},
+                ]
+            },
+            transformer.transform("", my_dict={"price": "a, ", "name": ",two"}),
+        )
+        self.assertEqual(
+            {"Line-item(s)": [{"name": "one"}, {"name": "two"}]},
+            transformer.transform("", my_dict={"": "a,b", "name": "one,two"}),
+        )

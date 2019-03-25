a3b9281d2c793d849c7d8a0fe17a8aaacf9c61af
Kirk Godtfredsen <kirk.godtfredsen@zapier.com>
2019-03-25 07:41:14 -0700

    Temp file creation and using seek to get back to the top, this feels better.

--- transformer/transforms/util/import_csv.py
+++ transformer/transforms/util/import_csv.py
@@ -1,7 +1,8 @@
 from transformer.registry import register
 from transformer.transforms.base import BaseTransform
 import csv
-import urllib2
+import urllib
+import tempfile
 
 class UtilImportCSVTransform(BaseTransform):
 
@@ -17,24 +18,44 @@ class UtilImportCSVTransform(BaseTransform):
     noun = "CSV"
     verb = "import"
 
-    def transform(self, csv_url, line_items=true, **kwargs):
+
+    def transform(self, csv_url, options=None, **kwargs):
         # Take a file input and output a set of line-item fields, or a big string field
 
         if not csv_url:
             return u''
 
+        if options is None:
+            options = {}
+        else:
+            string_output = options.get('line_items')
+
         input_key = "Line-item(s)"
         url = csv_url
-        response1 = urllib2.urlopen(url)
-        response2 = urllib2.urlopen(url)
-        response3 = urllib2.urlopen(url)
-        header = csv.Sniffer().has_header(response2.read(1024))
-        dialect = csv.Sniffer().sniff(response3.read(1024))
+        response = tempfile.NamedTemporaryFile()
+        urllib.urlretrieve(url, response.name)
+        header = csv.Sniffer().has_header(response.read(1024))
+        response.seek(0)
+        dialect = csv.Sniffer().sniff(response.read(1024))
+        response.seek(0)
+
+        output = {input_key: [], "header": header, "dialect": str(dialect)}
         this_line_item = []
-        csvreader = csv.DictReader(response1, dialect=dialect)
-        for row in csvreader:
-            this_line_item.append(row)
-        return {input_key: this_line_item}
+        
+        if header:
+            # we have headers
+            csvreader = csv.DictReader(response, dialect=dialect)
+            for row in csvreader:
+                this_line_item.append(row)
+            output[input_key] = this_line_item
+        else:
+            # we don't have headers, so need some fake LI keys, but need number of fields....
+            csvreader = csv.DictReader(response, dialect=dialect)
+            for row in csvreader:
+                this_line_item.append(row)
+            output[input_key] = this_line_item
+        response.close()
+        return output
 
     def fields(self, *args, **kwargs):
         return [

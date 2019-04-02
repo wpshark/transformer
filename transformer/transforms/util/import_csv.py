3be44b36d0ebc92394d7d253d40b7931431ee46c
Kirk Godtfredsen <kirk.godtfredsen@zapier.com>
2019-04-02 07:18:13 -0700

    Added check of line-items output true/false, and output a string if false

--- transformer/transforms/util/import_csv.py
+++ transformer/transforms/util/import_csv.py
@@ -5,7 +5,7 @@ import urllib
 import tempfile
 import os
 
-MAX_CSV_FILE_SIZE = 25e6
+MAX_CSV_FILE_SIZE = 50000
 
 class UtilImportCSVTransform(BaseTransform):
 
@@ -15,7 +15,7 @@ class UtilImportCSVTransform(BaseTransform):
     # Flesh this out to describe more functions
     help_text = (
         "Import a csv file from a public URL, File field from another Zap step, or entered text. "
-        "Limited to 500k/1000 rows "
+        "Limited to 500k/1000 rows. "
         "More on using csv files [here] (https://zapier.com/help/formatter/#how-process-csvs-formatter)"
     )
 
@@ -30,18 +30,18 @@ class UtilImportCSVTransform(BaseTransform):
             "label": "CSV file",
         }
 
-    def transform(self, csv_url, options=None, **kwargs):
+    def transform(self, csv_url, line_items=None, **kwargs):
         # Take a file input and output a set of line-item fields, or a big string field
 
         if not csv_url:
             return u''
 
-        if options is None:
-            options = {}
+        if line_items is None:
+            li_output = True
         else:
-            string_output = options.get('line_items')
+            li_output = line_items
 
-        input_key = "Line-item(s)"
+
         url = csv_url
         response = tempfile.NamedTemporaryFile()
 
@@ -53,32 +53,45 @@ class UtilImportCSVTransform(BaseTransform):
         size = response.tell()
         size_in_K = size / 1000
 
+        if (size > MAX_CSV_FILE_SIZE):
+            self.raise_exception('CSV Import only supports file sizes < 500K.')
+
+
         response.seek(0)
         header = csv.Sniffer().has_header(response.read())
         response.seek(0)
         dialect = csv.Sniffer().sniff(response.read())
         response.seek(0)
 
-        output = {input_key: [], "header": header, "size": size_in_K}
-        this_line_item = []
-
-        if header:
+        if li_output:
+            # output a line-item
+            input_key = "Line-item(s)"
+            if header:
             # we have headers
-            csvreader = csv.DictReader(response, dialect=dialect)
-            for row in csvreader:
-                this_line_item.append(row)
-            output[input_key] = this_line_item
+                output = {input_key: [], "header": header, "size": str(size_in_K) + 'K', "line item output": li_output}
+                this_line_item = []
+                csvreader = csv.DictReader(response, dialect=dialect)
+                for row in csvreader:
+                    this_line_item.append(row)
+                output[input_key] = this_line_item
+            else:
+                # we don't have headers, so need some fake LI keys, but need number of fields....
+                headerreader = csv.reader(response, dialect=dialect)
+                row1 = headerreader.next()
+                fieldnames = { 'Item {}'.format(i + 1): s for i, s in enumerate(row1)}
+                # now we have field names as Item 1..n - lets hope row #1 has everything it needs
+                response.seek(0)
+                csvreader = csv.DictReader(response, fieldnames=fieldnames, dialect=dialect)
+                for row in csvreader:
+                    this_line_item.append(row)
+                output[input_key] = this_line_item
+
         else:
-            # we don't have headers, so need some fake LI keys, but need number of fields....
-            headerreader = csv.reader(response, dialect=dialect)
-            row1 = headerreader.next()
-            fieldnames = { 'Item {}'.format(i + 1): s for i, s in enumerate(row1)}
-            # now we have field names as Item 1..n - lets hope row #1 has everything it needs
-            response.seek(0)
-            csvreader = csv.DictReader(response, fieldnames=fieldnames, dialect=dialect)
-            for row in csvreader:
-                this_line_item.append(row)
-            output[input_key] = this_line_item
+            #output is a string
+            input_key = "CSV Text"
+            output = {input_key: "", "header": header, "size": str(size_in_K) + 'K',"line item output": li_output}
+            output[input_key] = response.read()
+
         response.close()
         return output
 

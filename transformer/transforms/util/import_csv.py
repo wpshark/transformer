c49b50cfc8eed5e4440b2831931922947521072c
Kirk Godtfredsen <kirk.godtfredsen@zapier.com>
2019-04-18 18:30:57 -0700

    Cleaning up based on Tim's comments, and added some more tests, and a try/except for files that the Python csv reader can't parse.

--- transformer/transforms/util/import_csv.py
+++ transformer/transforms/util/import_csv.py
@@ -15,7 +15,7 @@ class UtilImportCSVTransform(BaseTransform):
     name = "import_csv"
     label = "Import CSV File"
     help_text = (
-        "Import a csv file from a public URL, File field from another Zap step, or entered text. "
+        "Import a CSV file from a public URL, File field from another Zap step, or entered text. "
         "Limited to 150k/1000 rows. "
         "More on using csv files [here] (https://zapier.com/help/formatter/#how-process-csvs-formatter)"
     )
@@ -31,16 +31,11 @@ class UtilImportCSVTransform(BaseTransform):
             "label": "CSV file",
         }
 
-    def transform(self, csv_url, line_items=None, **kwargs):
+    def transform(self, csv_url, line_items, **kwargs):
         # Take a file input and output a set of line-item fields, or a big string field
 
         if not csv_url:
             return u''
-
-        if line_items is None:
-            li_output = True
-        else:
-            li_output = line_items
 
         # create a temp file, then load the csv into it
         url = csv_url
@@ -56,18 +51,22 @@ class UtilImportCSVTransform(BaseTransform):
 
         # use csv utils to see if there is a header, and get the dialect (format) of the csv
         response.seek(0)
-        header = csv.Sniffer().has_header(response.read())
+        try:
+            header = csv.Sniffer().has_header(response.read())
+        except:
+            self.raise_exception('This CSV file has a format the Formatter does not understand')
+
         response.seek(0)
         dialect = csv.Sniffer().sniff(response.read())
         response.seek(0)
 
-        if li_output:
+        if line_items:
             # output line-items
             input_key = "Line-item(s)"
+            this_line_item = []
+            output = {input_key: [], "header": header, "line-item output": line_items}
             if header:
             # we have headers
-                output = {input_key: [], "header": header, "line item output": li_output}
-                this_line_item = []
                 csvreader = csv.DictReader(response, dialect=dialect)
                 for row in csvreader:
                     this_line_item.append(row)
@@ -87,7 +86,7 @@ class UtilImportCSVTransform(BaseTransform):
         else:
             #output is a big string
             input_key = "CSV Text"
-            output = {input_key: "", "header": header,"line item output": li_output}
+            output = {input_key: "", "header": header,"line-item output": line_items}
             output[input_key] = response.read()
 
         response.close()
@@ -98,9 +97,10 @@ class UtilImportCSVTransform(BaseTransform):
             {
                 "type": "boolean",
                 "required": False,
+                "default": "yes",
                 "key": "line_items",
                 "label": "Line-items",
-                "help_text": "By default, the csv file will be imported into line-item fields. Select No to import into a text field instead."
+                "help_text": "By default, the CSV file will be imported into line-item fields. Select No to import into a text field instead."
             }
 
         ]
--- transformer/transforms/util/import_csv_test.py
+++ transformer/transforms/util/import_csv_test.py
@@ -6,33 +6,78 @@ class TestUtilImportCSVTransform(unittest.TestCase):
 
     def test_import_csv(self):
         transformer = import_csv.UtilImportCSVTransform()
+        # UTF-8 - text
         self.assertEqual(
         {
             "CSV Text": "\xc3\xb8o,\xc3\xa9e,\xc3\xbcu\n1,2,3\n4,5,6\n8,9,10\n",
             "header": True,
-            "line item output": False
+            "line-item output": False
         },
         transformer.transform('https://drive.google.com/uc?export=download&id=1R_Pr0qaIUUVNO8c0oHoFnNtJl3el86xw', False))
+        # UTF-8 - line-item
         self.assertEqual(
         {
             "Line-item(s)": [{"\xc3\xb8o":"1","\xc3\xa9e":"2","\xc3\xbcu":"3"},
             {"\xc3\xb8o":"4","\xc3\xa9e":"5","\xc3\xbcu":"6"},
             {"\xc3\xb8o":"8","\xc3\xa9e":"9","\xc3\xbcu":"10"}],
             "header": True,
-            "line item output": True
+            "line-item output": True
         },
         transformer.transform('https://drive.google.com/uc?export=download&id=1R_Pr0qaIUUVNO8c0oHoFnNtJl3el86xw', True))
+        # UTF-8 - no csv, just text
         self.assertEqual(
         {
             "CSV Text": "garbage stuff here.....",
             "header": True,
-            "line item output": False
+            "line-item output": False
         },
         transformer.transform('https://drive.google.com/uc?export=download&id=14lvE6KkQzFvbl0bd8DOoW-12aTRUHEQH', False))
+        # UTF-8 - no csv, just text
         self.assertEqual(
         {
             "Line-item(s)": [],
             "header": True,
-            "line item output": True
+            "line-item output": True
         },
         transformer.transform('https://drive.google.com/uc?export=download&id=14lvE6KkQzFvbl0bd8DOoW-12aTRUHEQH', True))
+        # no headers - values missing, but all delimited
+        self.assertEqual(
+        {"Line-item(s)": [
+      {
+        "Item 1": "3",
+        "Item 2": "2",
+        "Item 3": "1",
+        "Item 4": "5",
+        "Item 5": "4"
+      },
+      {
+        "Item 1": "7",
+        "Item 2": "6",
+        "Item 3": "5",
+        "Item 4": "8",
+        "Item 5": ""
+      },
+      {
+        "Item 1": "11",
+        "Item 2": "",
+        "Item 3": "9",
+        "Item 4": "12",
+        "Item 5": ""
+      },
+      {
+        "Item 1": "14",
+        "Item 2": "",
+        "Item 3": "",
+        "Item 4": "16",
+        "Item 5": "15"
+      }
+    ],
+    "header": False,
+    "line-item output": True
+  },
+        transformer.transform('https://drive.google.com/uc?export=download&id=1I-t5L1KiDu4RDZCFTW2ydzJNwlfwm4Ld', True))
+        # no headers - values missing, but not all delimited
+        self.assertEqual(
+        {"Error: Could not determine delimiter"
+        },
+        transformer.transform('https://drive.google.com/uc?export=download&id=1fTzQP3GhlJIVzubhr_MJRv1pbosE6LsM', True))

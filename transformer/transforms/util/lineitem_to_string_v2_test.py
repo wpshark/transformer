0094a6435e1758f97e20e6c4da74b5c491a60adc
Tim Schafli <tim.schafli@zapier.com>
2019-05-22 11:42:28 -0700

    Re-write unit tests for new outputs

--- transformer/transforms/util/lineitem_to_string_v2_test.py
+++ transformer/transforms/util/lineitem_to_string_v2_test.py
@@ -3,39 +3,113 @@ import lineitem_to_string_v2
 
 
 class TestUtilLineItemToStringV2Transform(unittest.TestCase):
-
     def test_lineitem_to_string_v2_empty(self):
         transformer = lineitem_to_string_v2.UtilLineItemToStringV2Transform()
 
-        self.assertEqual('', transformer.transform_many([], options={'separator':','}))
-        self.assertEqual('', transformer.transform_many([''], options={'separator':','}))
+        self.assertEqual(
+            {"text_output": "", "item 1": ""},
+            transformer.transform_many([], options={"separator": ","}),
+        )
+        self.assertEqual(
+            {"text_output": "", "item 1": ""},
+            transformer.transform_many([""], options={"separator": ","}),
+        )
+        self.assertEqual(
+            {"text_output": "", "item 1": ""}, transformer.transform_many("")
+        )
 
     def test_lineitem_to_string_v2_many_empty(self):
         transformer = lineitem_to_string_v2.UtilLineItemToStringV2Transform()
 
-        self.assertEqual(',c,d', transformer.transform_many(['', 'c', 'd'], options={'separator':','}))
-        self.assertEqual(',,c,d', transformer.transform_many(['', '', 'c,d'], options={'separator':','}))
-        self.assertEqual(',', transformer.transform_many(['', ''], options={'separator':','}))
-        self.assertEqual('c,d,', transformer.transform_many(['c', 'd', ''], options={'separator':','}))
-        self.assertEqual(',,c,d', transformer.transform_many(['', '', 'c,d'], options={'separator':','}))
-        self.assertEqual(',', transformer.transform_many(['', ''], options={'separator':','}))
+        self.assertEqual(
+            {"text_output": ",c,d", "item 1": "", "item 2": "c", "item 3": "d"},
+            transformer.transform_many(["", "c", "d"], options={"separator": ","}),
+        )
+        self.assertEqual(
+            {"text_output": ",,c,d", "item 1": "", "item 2": "", "item 3": "c,d"},
+            transformer.transform_many(["", "", "c,d"], options={"separator": ","}),
+        )
+        self.assertEqual(
+            {"text_output": ",", "item 1": "", "item 2": ""},
+            transformer.transform_many(["", ""], options={"separator": ","}),
+        )
+        self.assertEqual(
+            {"text_output": "c,d,", "item 1": "c", "item 2": "d", "item 3": ""},
+            transformer.transform_many(["c", "d", ""], options={"separator": ","}),
+        )
 
     def test_lineitem_to_string_v2_one(self):
         transformer = lineitem_to_string_v2.UtilLineItemToStringV2Transform()
-        self.assertEqual('a', transformer.transform_many(['a'], options={'separator':','}))
+        self.assertEqual(
+            {"text_output": "a", "item 1": "a"},
+            transformer.transform_many(["a"], options={"separator": ","}),
+        )
 
     def test_lineitem_to_string_v2_many(self):
         transformer = lineitem_to_string_v2.UtilLineItemToStringV2Transform()
-        self.assertEqual('a,b,c,d', transformer.transform_many(['a,b', 'c,d'], options={'separator':','}))
-        self.assertEqual('a,b,c,d', transformer.transform_many(['a', 'b', 'c', 'd'], options={'separator':','}))
-        self.assertEqual('a,b,c,d', transformer.transform_many(['a,b,c,d'], options={'separator':','}))
+        self.assertEqual(
+            {"text_output": "a,b,c,d", "item 1": "a,b", "item 2": "c,d"},
+            transformer.transform_many(["a,b", "c,d"], options={"separator": ","}),
+        )
+        self.assertEqual(
+            {
+                "text_output": "a,b,c,d",
+                "item 1": "a",
+                "item 2": "b",
+                "item 3": "c",
+                "item 4": "d",
+            },
+            transformer.transform_many(
+                ["a", "b", "c", "d"], options={"separator": ","}
+            ),
+        )
+        self.assertEqual(
+            {"text_output": "a,b,c,d", "item 1": "a,b,c,d"},
+            transformer.transform_many(["a,b,c,d"], options={"separator": ","}),
+        )
 
     def test_lineitem_to_string_v2_other_separator(self):
         transformer = lineitem_to_string_v2.UtilLineItemToStringV2Transform()
-        self.assertEqual('a b c d', transformer.transform_many(['a', 'b', 'c', 'd'], options={'separator':'[:space:]'}))
-        self.assertEqual('a b c d', transformer.transform_many(['a', 'b', 'c', 'd'], options={'separator':'[:s:]'}))
-        self.assertEqual('a;b;c;d', transformer.transform_many(['a', 'b', 'c', 'd'], options={'separator':';'}))
+        self.assertEqual(
+            {
+                "text_output": "a b c d",
+                "item 1": "a",
+                "item 2": "b",
+                "item 3": "c",
+                "item 4": "d",
+            },
+            transformer.transform_many(
+                ["a", "b", "c", "d"], options={"separator": "[:space:]"}
+            ),
+        )
+        self.assertEqual(
+            {
+                "text_output": "a b c d",
+                "item 1": "a",
+                "item 2": "b",
+                "item 3": "c",
+                "item 4": "d",
+            },
+            transformer.transform_many(
+                ["a", "b", "c", "d"], options={"separator": "[:s:]"}
+            ),
+        )
+        self.assertEqual(
+            {
+                "text_output": "a;b;c;d",
+                "item 1": "a",
+                "item 2": "b",
+                "item 3": "c",
+                "item 4": "d",
+            },
+            transformer.transform_many(
+                ["a", "b", "c", "d"], options={"separator": ";"}
+            ),
+        )
 
     def test_lineitem_to_string_v2_nolineitem(self):
         transformer = lineitem_to_string_v2.UtilLineItemToStringV2Transform()
-        self.assertEqual('abcd', transformer.transform_many('abcd', options={'separator':'[:space:]'}))
+        self.assertEqual(
+            {"text_output": "abcd", "item 1": "abcd"},
+            transformer.transform_many("abcd", options={"separator": "[:space:]"}),
+        )

d349782d563350e09db690ddd59579dd445bef2a
Kirk Godtfredsen <kirk.godtfredsen@zapier.com>
2019-08-16 09:09:18 -0700

    Updated to Tylers most recent regex for testing

--- transformer/transforms/string/phone_number_extract_v2.py
+++ transformer/transforms/string/phone_number_extract_v2.py
@@ -6,8 +6,8 @@ from transformer.transforms.base import BaseTransform
 # Regex based on second script in question here (https://stackoverflow.com/questions/3868753/find-phone-numbers-in-python-script)
 # Tweaked to accept international phone numbers, including 2 digit country codes and area codes, along with area codes that begin with a 0: https://gist.github.com/maguay/f3a46f578568a608413530e27b78af88
 # old URL_REGEX = r"""(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?"""
-#Tyler's update: https://regex101.com/r/wLoxlp/1
-URL_REGEX =r"""(?:[^.\n\r][-._\/ ]?[\(\+]?(\d{1,5})\)?[^.\n\r]){2,5}"""
+#Tyler's update: https://regex101.com/r/wLoxlp/9
+URL_REGEX =r"""(?:[\(\+]?(\d{1,5})[-_\/ \)]{0,4}){2,5}((ex|ext|extension|x)\.?[-\s]?\d{1,5})?"""
 
 class StringPhoneExtractV2Transform(BaseTransform):
 

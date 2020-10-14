0cbfa154d5f2e3fc7fd4fe44ce8fdc8d094f5b7b
Kirk Godtfredsen <kirk.godtfredsen@zapier.com>
2020-10-14 14:54:13 -0700

    going crazy and making changes directly to develop

--- transformer/transforms/string/phone_number_extract_v2.py
+++ transformer/transforms/string/phone_number_extract_v2.py
@@ -4,25 +4,50 @@ from transformer.registry import register
 from transformer.transforms.base import BaseTransform
 
 # Regex based on Kristie's regex here: https://regex101.com/r/1bsTTG/47
-URL_REGEX = r"(?:\+?\d{8}(?:\d{1,5})?|\(\+?\d{2,3}\)\s?(?:\d{4}[\s*.-]?\d{4}|\d{3}[\s*.-]?\d{3,4}|\d{2}([\s*.-]?)\d{2}\1\d{2}(?:\1\d{2})?))|((\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4})|\(?\+?\d{1,4}?\)?[-.\s]?\(?\d{1,3}?\)?([-.\s]?\d{1,9}){3,6}"
-
+URL_REGEX_ALL = r"(?:\+?\d{8}(?:\d{1,5})?|\(\+?\d{2,3}\)\s?(?:\d{4}[\s*.-]?\d{4}|\d{3}[\s*.-]?\d{3,4}|\d{2}([\s*.-]?)\d{2}\1\d{2}(?:\1\d{2})?))|((\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4})|\(?\+?\d{1,3}?\)?[-.\s*]?\(?\d{1,3}?\)?[-.\s*]([-.\s*]?\d{1,9}){3,6}"
+URL_REGEX_NA = r"((\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4})"
+URL_REGEX_IN = r"(?:\+?\d{8}(?:\d{1,5})?|\(\+?\d{2,3}\)\s?(?:\d{4}[\s*.-]?\d{4}|\d{3}[\s*.-]?\d{3,4}|\d{2}([\s*.-]?)\d{2}\1\d{2}(?:\1\d{2})?))"
 
 class StringPhoneExtractV2Transform(BaseTransform):
 
     category = 'string'
     name = 'phone_extract_v2'
     label = 'Extract Phone Number'
-    help_text = ('Find and copy a complete phone number out of a text field. Finds the first phone number only. '
-    'If numbers can\'t be found consistently, try the [Extract Pattern](https://zapier.com/help/create/format/find-text-with-regex-in-zaps) transform.')
+    help_text = 'Find and copy a complete phone number out of a text field. Finds the first phone number only.'
+    
     noun = 'Text'
     verb = 'find and copy a phone number from'
 
-    def transform(self, str_input, **kwargs):
+    def transform(self, str_input, regex='all', **kwargs):
         if isinstance(str_input, basestring):
-            match = re.search(URL_REGEX, str_input)
-            return match.group(0) if match else u''
+            if regex=='na':
+                match = re.search(URL_REGEX_NA, str_input)
+                return match.group(0) if match else u''
+            if regex=='in':
+                match = re.search(URL_REGEX_IN, str_input)
+                return match.group(0) if match else u''
+            else:
+                match = re.search(URL_REGEX_ALL, str_input)
+                return match.group(0) if match else u''
         else:
             return u''
+
+    def fields(self, *args, **kwargs):
+        return [
+            {
+                "type": "unicode",
+                "required": False,
+                "key":  "regex",
+                "choices": "all|All Formats,na|North American Number Plan,in|International Format (Standard)",
+                "label": "Phone Number Format",
+                "default": "all",
+                "help_text": (
+                    'By default, we\'ll use the most aggressive search, which could produce false positives. '
+                    'If you find this, you can try the other options, or if numbers can\'t be found consistently, ' 
+                    'try the [Extract Pattern](https://zapier.com/help/create/format/find-text-with-regex-in-zaps) transform.'),
+         
+            },
+        ]
 
 
 register(StringPhoneExtractV2Transform())
--- transformer/transforms/string/phone_number_extract_v2_test.py
+++ transformer/transforms/string/phone_number_extract_v2_test.py
@@ -8,6 +8,6 @@ class TestStringPhoneExtractV2Transform(unittest.TestCase):
         self.assertEqual(transformer.transform(None), "")
         self.assertEqual(transformer.transform("This is a test without a phone number"), "")
         self.assertEqual(transformer.transform("my phone number is 123-456-7890"), "123-456-7890")
-        self.assertEqual(transformer.transform("bob said to call him at and 321-0989."), "")
+        self.assertEqual(transformer.transform("bob said to call him at and 321-0989."), "321-0989")
         self.assertEqual(transformer.transform("The post office's number is (800) 777-9988 and it costs $53 a month"), "(800) 777-9988")
         self.assertEqual(transformer.transform("Call bob at +66.81.234.5566 tomorrow at 2 PM"), "+66.81.234.5566")

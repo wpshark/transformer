3cc92cee86046fe4a05dd68319250c37663d372b
kgodtfredsen <kirk.godtfredsen@zapier.com>
2020-10-15 15:14:34 -0700

    Phone again  (#126)
    * update with new thoughts, keep old transform and add a default + options for other regexs

--- transformer/transforms/string/phone_number_extract.py
+++ transformer/transforms/string/phone_number_extract.py
@@ -3,9 +3,14 @@ import re
 from transformer.registry import register
 from transformer.transforms.base import BaseTransform
 
-# Regex based on second script in question here (https://stackoverflow.com/questions/3868753/find-phone-numbers-in-python-script)
+# Orignal Regex based on second script in question here (https://stackoverflow.com/questions/3868753/find-phone-numbers-in-python-script)
 # Tweaked to accept international phone numbers, including 2 digit country codes and area codes, along with area codes that begin with a 0: https://gist.github.com/maguay/f3a46f578568a608413530e27b78af88
-# updated to include seperate regexes - testing done at https://regex101.com/r/1bsTTG/51
+# updated to include seperate regexes - testing is much easier here:
+# orig: https://regex101.com/r/FCS4px/1
+# all: https://regex101.com/r/sk6MVY/1
+# na: https://regex101.com/r/QYWyPc/2
+# in: https://regex101.com/r/DVkuoA/1
+
 URL_REGEX_ORIG = r"""(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?"""
 URL_REGEX_ALL = r"(?:\+?\d{8}(?:\d{1,5})?|\(\+?\d{2,3}\)\s?(?:\d{4}[\s*.-]?\d{4}|\d{3}[\s*.-]?\d{3,4}|\d{2}([\s*.-]?)\d{2}\1\d{2}(?:\1\d{2})?))|((\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4})|\(?\+?\d{1,3}?\)?[-.\s*]?\(?\d{1,3}?\)?[-.\s*]([-.\s*]?\d{1,9}){3,6}"
 URL_REGEX_NA = r"(?:\+?\d{10,11}?(?=\s)|(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4})"
@@ -45,13 +50,13 @@ class StringPhoneExtractTransform(BaseTransform):
                 "type": "unicode",
                 "required": False,
                 "key":  "regex",
-                "choices": "orig|Original (Most NANP and International),na|North American Number Plan (NANP),in|International,all|All Formats,",
+                "choices": "orig|Original (NANP and International),na|North American Number Plan (NANP),in|International,all|Universal,",
                 "label": "Phone Number Format",
                 "default": "orig",
                 "help_text": (
-                    'By default, we\'ll use our original search, which is good for most NANPs and many International numbers. '
-                    'If you find this does not work, you can try the other options for a specific format (NAMP and International), ' 
-                    'or "All", which will find most numbers, but also can result in false positives.')
+                    'By default, we\'ll use our original search, which will find many NANP and International numbers. '
+                    'If this does not work consistently, try a specific format (NAMP and International), ' 
+                    'or "Universal", which will find most phone numbers, but also can result in false positives.')
          
             },
         ]
